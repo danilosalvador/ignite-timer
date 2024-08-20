39ca02956cfbe67252700826a43908768d7e3460
Danilo Salvador <danilo.salvador@smartlogic.com.br>
2024-08-20 17:33:26 -0300

    Páginas e rotas - Componente: Header

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,14 +5,14 @@ import { History } from './pages/History'
 import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<DefaultLayout />}>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/history' element={<History />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DefaultLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/history" element={<History />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
